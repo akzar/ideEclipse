558ea8f71abbef9ff12647d3b0c64834343fafd1
akzar <akzar@DESKTOP-73P3VVJ>
2020-03-12 23:08:35 -0300

    Incluido alguamas variáveis na classe Fabricante;

--- oficinaSenac/src/model/Fabricante.java
+++ oficinaSenac/src/model/Fabricante.java
@@ -2,6 +2,7 @@ package model;
 
 public class Fabricante {
 	private int codFabricante, idFabricante;
-	private String nomeFabricante;
+	private char dddFabricante;
+	private String nomeFabricante, emailFabricante, telefoneFabricante;
 
 }
